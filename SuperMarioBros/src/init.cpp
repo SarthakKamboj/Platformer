2c563dcdfdf8d3c709286a2286a509e8f7ab39d0
SarthakKamboj <sarthakkamboj22@gmail.com>
2023-05-30 23:31:48 -0700

    renamed and added some resources

---
+++ SuperMarioBros/src/init.cpp
@@ -0,0 +1,44 @@
+#include "SDL.h"
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include "glad/glad.h"
+#include "constants.h"
+
+void init() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		const char* sdl_error = SDL_GetError();
+		std::string error_msg = "SDL could not be initialized: " + std::string(sdl_error);
+		throw std::runtime_error(error_msg);
+	}
+
+	std::cout << "SDL loaded properly" << std::endl;
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+	SDL_Window* window = SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+
+	if (window == NULL) {
+		const char* sdl_error = SDL_GetError();
+		std::string error_msg = "SDL could not be initialized: " + std::string(sdl_error);
+		throw std::runtime_error(error_msg);
+	}
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+	gladLoadGLLoader(SDL_GL_GetProcAddress);
+
+	SDL_GL_MakeCurrent(window, context);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
